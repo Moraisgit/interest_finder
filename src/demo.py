5fe30faf0ad7603a53ef26dd32b5455c783f487b
Moraisgit <vascocarvalho18@gmail.com>
2025-03-27 15:26:28 +0100

    Steps on prompts and termiate
    Tried to make steps on the prompts so agents follow them more precisely. When an agent replys with terminate the conversation ends. Can use another model llama.

--- src/demo.py
+++ src/demo.py
@@ -1,10 +1,14 @@
 import autogen
 
-def termination_message(msg):
-    return "TERMINATE" in str(msg.get("content", ""))
+# llm_config = {"config_list": [{
+#     "model": "hermes-3-llama-3.2-3b", 
+#     "base_url": "http://localhost:1234/v1",
+#     "api_key": "NULL", # just a placeholder
+#     "price": [0, 0],  # No cost for local models
+# }]}
 
 llm_config = {"config_list": [{
-    "model": "hermes-3-llama-3.2-3b", 
+    "model": "llama-3.2-3b-instruct", 
     "base_url": "http://localhost:1234/v1",
     "api_key": "NULL", # just a placeholder
     "price": [0, 0],  # No cost for local models
@@ -18,27 +22,42 @@ termination_notice = (
 
 cathy = autogen.ConversableAgent(
     name="Cathy",
-    system_message="""Your name is Cathy and you are a guest to a party. You will be greeted by a butler when you arrive. 
-    The butler will ask you questions to which you have to respond in a clear way; he will also make small talk to which you have to
-    continue to answer and keep the conversation going. Your favourite drink is Cola. Your interest is to skateboard.""" + termination_notice,
+    system_message="""Your name is Cathy and you are a guest to a party. 
+    You will be greeted by a butler when you arrive and he will take you to your seat. 
+
+    DETAILS ABOUT YOU:
+    - Favourite drink: Cola.
+    - Interests at the party: Find people to skateboard with, meet new people and drink.
+    
+    YOU MUST FOLLOW THESE STEPS WHEN TALKING WITH THE BUTLER:
+    1. Answer to the butler's questions in a clear way!
+    2. Follow the butler to an empty seat while answering other questions from the butler.""" + termination_notice,
     llm_config=llm_config,
     human_input_mode="NEVER",  # Never ask for human input.
+    is_termination_msg=lambda msg: "terminate" in msg["content"].lower()
 )
 
 bob = autogen.ConversableAgent(
     name="Bob",
-    system_message="""Your name is Bob and you are a butler at a party. Your job is to greet guests at the entrance of a party and take them 
-    to a free sit. When greeting guests you have to find out they're name and favourite drink. Then you guide the guests to a free sit. On the way 
-    to a free sit you have to find the interest of the guest by making small talk. You can consider that the conversation has ended when the guest is 
-    seated and you found every information required about the guest. At the end of the conversation, summarize what you discovered about your 
-    guest like this: 'Guest name: #; Favourite Drink: #; Interest: #.' (where you should fill the '#' with the information you gathered).""" + 
+    system_message="""Your name is Bob and you are a butler at a party. 
+    Your job is to greet guests at the entrance of a party, get to kown them and take them to a free sit.
+
+    YOU MUST FOLLOW THESE STEPS WHEN GREETING GUESTS: 
+    1. When greeting guests at the entrance you have to find out they're name and favourite drink. 
+    2. Guide the guests to a free sit by telling them to follow you, close to your left. 
+    3. On the way to a free sit you have to find the interest of the guest by making small talk.
+    
+    Consider that the conversation has ended when the guest is seated and you found every information required about the guest. 
+    At the end of the conversation, YOU MUST summarize what you discovered about your guest like this: 
+    'Guest name: #; Favourite Drink: #; Interest: #.' (where you should fill the '#' with the information you gathered).""" + 
     termination_notice,
     llm_config=llm_config,
     human_input_mode="NEVER",  # Never ask for human input.
+    is_termination_msg=lambda msg: "terminate" in msg["content"].lower()
 )
 
 result = bob.initiate_chat(recipient=cathy, 
                            message="Hi! I am Bob, what is your name?", 
-                           is_termination_msg=termination_message,
+                           is_termination_msg=lambda msg: "terminate" in msg["content"].lower(),
                            clear_history=True
 )
